6367294f7a0e55336d1e11b2bed794041f17b0b4
nashmagbanua <nashmagbanua@gmail.com>
2025-06-01 07:56:37 +0800

    Delete tools/boiler.js

--- tools/boiler.js
+++
@@ -1,9 +0,0 @@
-
-function calculateEfficiency() {
-  const steam = parseFloat(document.getElementById("steamOutput").value) || 0;
-  const buckets = parseFloat(document.getElementById("coalBuckets").value) || 0;
-  const gcv = parseFloat(document.getElementById("gcvValue").value) || 0;
-  const coalKg = buckets * 200;
-  const efficiency = (steam * 640) / (coalKg * gcv) * 100;
-  document.getElementById("efficiencyResult").textContent = efficiency.toFixed(2);
-}
